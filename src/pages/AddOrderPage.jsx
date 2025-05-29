c28daf668b385dd9da99eef9d57e4da3f63b492a
Yoseob Kim <ykim9611@gmail.com>
2025-05-29 14:52:49 -0700

    created supabase client configuration and made successful connection

--- src/pages/AddOrderPage.jsx
+++ src/pages/AddOrderPage.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { supabase } from '../services/supabaseClient';
 
 function AddOrderPage(props) {
+  useEffect(() => {
+    const fetchInventory = async () => {
+      const { data, error } = await supabase
+        .from('procurement_specialists')
+        .select('*');
+
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else {
+        console.log('Fetched data:', data);
+      }
+    };
+
+    fetchInventory();
+  }, []);
+
   return <div>Add Order Page</div>;
 }
 
