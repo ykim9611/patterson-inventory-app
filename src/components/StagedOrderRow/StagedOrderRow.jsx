9d4c32efbe40326c88d41e0aeb0dd483bcff4641
Yoseob Kim <ykim9611@gmail.com>
2025-06-03 23:05:03 -0700

    added deletion capability in staged orders

--- src/components/StagedOrderRow/StagedOrderRow.jsx
+++ src/components/StagedOrderRow/StagedOrderRow.jsx
@@ -8,134 +8,151 @@ function StagedOrderRow({
   handleInputChange,
   locations,
   procurementSpecialists,
+  handleDeleteClick,
 }) {
   return (
-    <tr>
-      <td>
-        <input
-          type='text'
-          value={stagedOrder.customerName}
-          onChange={(e) =>
-            handleInputChange(index, 'customerName', e.target.value)
-          }
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <select
-          name='procurementSpecialist'
-          value={stagedOrder.procurementSpecialist}
-          onChange={(e) =>
-            handleInputChange(index, 'procurementSpecialist', e.target.value)
-          }
-        >
-          {procurementSpecialists.map((ps) => (
-            <option key={ps.id} value={ps.procurement_specialist}>
-              {ps.procurement_specialist}
-            </option>
-          ))}
-        </select>
-      </td>
-      <td>
-        <input
-          type='text'
-          value={stagedOrder.salesOrder}
-          onChange={(e) =>
-            handleInputChange(index, 'salesOrder', e.target.value)
-          }
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <input
-          type='date'
-          value={stagedOrder.receivedDate}
-          onChange={(e) =>
-            handleInputChange(index, 'receivedDate', e.target.value)
-          }
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <input
-          type='text'
-          value={stagedOrder.partNumber}
-          onChange={(e) =>
-            handleInputChange(index, 'partNumber', e.target.value)
-          }
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <input
-          type='text'
-          value={stagedOrder.partDescription || ''}
-          onChange={(e) =>
-            handleInputChange(index, 'partDescription', e.target.value)
-          }
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <input
-          type='text'
-          value={stagedOrder.serialNumber}
-          onChange={(e) =>
-            handleInputChange(index, 'serialNumber', e.target.value)
-          }
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <input
-          type='text'
-          value={stagedOrder.quantity}
-          onChange={(e) => handleInputChange(index, 'quantity', e.target.value)}
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <input
-          type='text'
-          className={styles.tableBoxInput}
-          value={stagedOrder.boxNumber}
-          onChange={(e) =>
-            handleInputChange(index, 'boxNumber', e.target.value)
-          }
-        />{' '}
-        /{' '}
-        <input
-          type='text'
-          className={styles.tableBoxInput}
-          value={stagedOrder.boxNumberTotal}
-          onChange={(e) =>
-            handleInputChange(index, 'boxNumberTotal', e.target.value)
-          }
-        />
-      </td>
-      <td>
-        <input
-          type='text'
-          value={stagedOrder.notes || ''}
-          onChange={(e) => handleInputChange(index, 'notes', e.target.value)}
-          className={styles.tableInput}
-        />
-      </td>
-      <td>
-        <select
-          name='location'
-          value={stagedOrder.location || ''}
-          onChange={(e) => handleInputChange(index, 'location', e.target.value)}
-        >
-          <option value=''>Select a Location</option>
-          {locations.map((location) => (
-            <option key={location.id} value={location.location}>
-              {location.location}
-            </option>
-          ))}
-        </select>
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td>
+          <input
+            type='text'
+            value={stagedOrder.customerName}
+            onChange={(e) =>
+              handleInputChange(index, 'customerName', e.target.value)
+            }
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <select
+            name='procurementSpecialist'
+            value={stagedOrder.procurementSpecialist}
+            onChange={(e) =>
+              handleInputChange(index, 'procurementSpecialist', e.target.value)
+            }
+          >
+            {procurementSpecialists.map((ps) => (
+              <option key={ps.id} value={ps.procurement_specialist}>
+                {ps.procurement_specialist}
+              </option>
+            ))}
+          </select>
+        </td>
+        <td>
+          <input
+            type='text'
+            value={stagedOrder.salesOrder}
+            onChange={(e) =>
+              handleInputChange(index, 'salesOrder', e.target.value)
+            }
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <input
+            type='date'
+            value={stagedOrder.receivedDate}
+            onChange={(e) =>
+              handleInputChange(index, 'receivedDate', e.target.value)
+            }
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <input
+            type='text'
+            value={stagedOrder.partNumber}
+            onChange={(e) =>
+              handleInputChange(index, 'partNumber', e.target.value)
+            }
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <input
+            type='text'
+            value={stagedOrder.partDescription || ''}
+            onChange={(e) =>
+              handleInputChange(index, 'partDescription', e.target.value)
+            }
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <input
+            type='text'
+            value={stagedOrder.serialNumber}
+            onChange={(e) =>
+              handleInputChange(index, 'serialNumber', e.target.value)
+            }
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <input
+            type='text'
+            value={stagedOrder.quantity}
+            onChange={(e) =>
+              handleInputChange(index, 'quantity', e.target.value)
+            }
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <input
+            type='text'
+            className={styles.tableBoxInput}
+            value={stagedOrder.boxNumber}
+            onChange={(e) =>
+              handleInputChange(index, 'boxNumber', e.target.value)
+            }
+          />{' '}
+          /{' '}
+          <input
+            type='text'
+            className={styles.tableBoxInput}
+            value={stagedOrder.boxNumberTotal}
+            onChange={(e) =>
+              handleInputChange(index, 'boxNumberTotal', e.target.value)
+            }
+          />
+        </td>
+        <td>
+          <input
+            type='text'
+            value={stagedOrder.notes || ''}
+            onChange={(e) => handleInputChange(index, 'notes', e.target.value)}
+            className={styles.tableInput}
+          />
+        </td>
+        <td>
+          <select
+            name='location'
+            value={stagedOrder.location || ''}
+            onChange={(e) =>
+              handleInputChange(index, 'location', e.target.value)
+            }
+          >
+            <option value=''>Select a Location</option>
+            {locations.map((location) => (
+              <option key={location.id} value={location.location}>
+                {location.location}
+              </option>
+            ))}
+          </select>
+        </td>
+        <td>
+          <img
+            onClick={(e) => handleDeleteClick(e, index)}
+            alt=''
+            src='/red-trash-can-icon.svg'
+            width='20'
+            height='20'
+            className={styles.delete}
+          />
+        </td>
+      </tr>
+    </>
   );
 }
 
