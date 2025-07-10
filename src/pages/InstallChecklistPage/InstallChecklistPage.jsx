5ffd45db830d31d9185181c6b5891736e6bd4bc5
Yoseob Kim <ykim9611@gmail.com>
2025-07-10 14:36:03 -0700

    install checklist page on hold

--- src/pages/InstallChecklistPage/InstallChecklistPage.jsx
+++ src/pages/InstallChecklistPage/InstallChecklistPage.jsx
@@ -1,26 +1,66 @@
 import React, { useState } from 'react';
 import styles from './InstallChecklistPage.module.css';
-import Print from '../../components/Print';
+import { supabase } from '../../services/supabaseClient';
+import { AgGridReact } from 'ag-grid-react';
+import { viewColumnDefs } from '../../grid/viewColumnDefs';
+import MyComponent from '../../components/MyComponent';
 
 function InstallChecklistPage(props) {
   const [serviceOrderNumber, setServiceOrderNumber] = useState('');
+  const [foundTiedOrders, setFoundTiedOrders] = useState(false);
+  const [rowData, setRowData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleSearchServiceOrder = () => {
-    console.log(serviceOrderNumber);
+    console.log(typeof serviceOrderNumber);
+    const getTiedOrders = async () => {
+      const { data, error } = await supabase
+        .from('orders')
+        .select('*')
+        .eq('install_number', serviceOrderNumber);
+      // .order('sales_order', { ascending: false });
+      if (error) {
+        console.error('Error fetching data: ', error);
+      } else {
+        setFoundTiedOrders(true);
+        setRowData(data);
+        console.log(data);
+      }
+    };
+    getTiedOrders();
   };
 
   return (
     <div className={styles.container}>
-      <h2>Install Checklist</h2>
-      <div className={styles.installSearchInputContainer}>
-        <label labelfor='installSearchInput'>Enter Install Number: </label>
-        <input
-          name='installSearchInput'
-          type='text'
-          onChange={(e) => setServiceOrderNumber(e.target.value)}
-        />
-      </div>
-      <button onClick={handleSearchServiceOrder}>Find Install</button>
+      {!foundTiedOrders ? (
+        <>
+          <h2>Install Checklist</h2>
+          <div className={styles.installSearchInputContainer}>
+            <label labelfor='installSearchInput'>Enter Install Number: </label>
+            <input
+              name='installSearchInput'
+              type='text'
+              onChange={(e) => setServiceOrderNumber(e.target.value)}
+            />
+          </div>
+          <button onClick={handleSearchServiceOrder}>Find Install</button>
+        </>
+      ) : (
+        <div
+          className='ag-theme-alpine'
+          style={{ height: '70vh', width: '100%' }}
+        >
+          <AgGridReact
+            suppressMovableColumns={true}
+            loading={loading}
+            rowData={rowData}
+            columnDefs={viewColumnDefs}
+            enableCellTextSelection={true}
+            pagination={true}
+          />
+          <MyComponent />
+        </div>
+      )}
     </div>
   );
 }
